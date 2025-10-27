3b544424cd3e9a0fa62dfc134ae627e0dc3211bb
Kirill <stoporenko.k@gmail.com>
2025-10-27 21:52:19 +0700

    draft of the homework solution

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,4 +1,4 @@
-from json import loads
+import random
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
@@ -11,7 +11,7 @@ def test_post_v1_account():
     mailhog_api = MailhogApi(host="http://5.63.153.31:5025")
 
     # Регистрация нового пользователя
-    login = "arbuzz29"
+    login = f"arbuzz{int(random.random() * 10000000000000000)}"
     email = f"{login}@mail.ru"
     password = '123456789'
 
@@ -25,42 +25,3 @@ def test_post_v1_account():
     print(f"\n{response.status_code}")
     print(response.text)
     assert response.status_code == 201, f"Пользователь не был создан {response.json()}"
-
-    # Получить письма из почтового ящика
-    response = mailhog_api.get_api_v2_messages()
-    print(response.status_code)
-    print(response.text)
-    assert response.status_code == 200, f"Письма не были получены"
-
-    # Получить авторизационный токен
-    token = get_activation_token_by_login(login, response)
-    assert token is not None, f"Токен для пользователя {login} не был получен"
-
-    # Активация зарегистрированного пользователя
-    response = account_api.put_v1_account_token(token=token)
-    print(response.status_code)
-    print(response.text)
-    assert response.status_code == 200, "Пользователь не активирован"
-
-    # Авторизация пользователя
-    json_data = {
-        'login': login,
-        'password': password,
-        'rememberMe': True,
-    }
-
-    response = login_api.post_v1_account_login(json_data=json_data)
-    print(response.status_code)
-    print(response.text)
-    assert response.status_code == 200, "Пользователь не смог авторизоваться"
-
-def get_activation_token_by_login(login, response):
-    token = None
-    for item in response.json()['items']:
-        user_data = loads(item['Content']['Body'])
-        user_login = user_data['Login']
-        if user_login == login:
-            print(user_login)
-            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
-            print(token)
-    return token

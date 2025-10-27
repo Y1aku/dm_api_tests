a94c5bd1ae2c73dba2dd2f4363fdba689626e09c
Kirill <stoporenko.k@gmail.com>
2025-10-27 21:22:52 +0700

    Написали джобу для запуска тестов

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -11,7 +11,7 @@ def test_post_v1_account():
     mailhog_api = MailhogApi(host="http://5.63.153.31:5025")
 
     # Регистрация нового пользователя
-    login = "arbuzz28"
+    login = "arbuzz29"
     email = f"{login}@mail.ru"
     password = '123456789'
 
